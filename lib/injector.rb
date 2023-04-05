59d3cda4865e5867ce26cea8fe0535e411921276
Leon Cruz <leon.cruz.teixeira@gmail.com>
2023-04-05 11:08:17 -0300

    feat: add "attr_injector" class method to search for dependency and inject method into included class

--- lib/injector.rb
+++ lib/injector.rb
@@ -1,8 +1,26 @@
 # frozen_string_literal: true
 
 require_relative 'injector/version'
+require_relative 'injector/dependencies'
+require_relative 'injector/injectable'
+require_relative 'injector/inject'
 
+# Entrypoint module
 module Injector
-  class Error < StandardError; end
-  # Your code goes here...
+  def self.included(target)
+    target.extend(ClassMethods)
+  end
+
+  # methods to add on included class
+  module ClassMethods
+    def attr_injector(name)
+      injectable = Inject.find(name)
+
+      define_method name do
+        instance_variable_set(:"@#{name}", injectable.callback.call) unless instance_variable_defined?(:"@#{name}")
+
+        instance_variable_get(:"@#{name}")
+      end
+    end
+  end
 end
--- spec/injector_spec.rb
+++ spec/injector_spec.rb
@@ -1,7 +1,56 @@
 # frozen_string_literal: true
 
 RSpec.describe Injector do
-  it 'has a version number' do
-    expect(Injector::VERSION).to be '0.0.1'
+  describe '.included' do
+    context 'when Injector is included into class' do
+      let(:dummy_class) { Class.new { include Injector } }
+
+      it 'has to insert on class the ClassMethods as an ancestor' do
+        expect(dummy_class.methods.include?(:attr_injector)).to be true
+      end
+    end
+  end
+
+  describe '.attr_injector' do
+    context 'when exist a instance for given injector name' do
+      let(:service_class) { Class.new }
+
+      let(:service_dependencies) do
+        Class.new do
+          extend Injector::Dependencies
+
+          register :service, -> { ServiceClass.new }
+        end
+      end
+
+      let(:dummy_class) do
+        Class.new do
+          include Injector
+
+          attr_injector :service
+        end
+      end
+
+      before do
+        stub_const 'ServiceClass', service_class
+        stub_const 'ServiceDependencies', service_dependencies
+      end
+
+      it 'when exist an instance for given name has to retrieve lazyly' do
+        expect(dummy_class.new.service).to be_instance_of service_class
+      end
+    end
+
+    context 'when a instance for given injector name was not registered' do
+      it 'has to raise exception' do
+        expect do
+          Class.new do
+            include Injector
+
+            attr_injector :service
+          end
+        end.to raise_error Injector::InjectableNotFound
+      end
+    end
   end
 end
